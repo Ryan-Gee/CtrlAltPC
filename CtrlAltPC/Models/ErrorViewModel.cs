2304d8a1f0371e334e951f8f90c9e57c58f63595
Ryan-Gee <200396244@student.georgianc.on.ca>
2019-09-10 10:56:05 -0400

    Lesson 1: Project Setup

---
+++ CtrlAltPC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CtrlAltPC.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
